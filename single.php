37dcf5782ba7d017c2a843e3f266b1a448cbfb77
william Harty <harryalph9@gmail.com>
2022-07-25 13:32:19 +0100

    added some slashes to the search.php and the single.php pages

--- single.php
+++ single.php
@@ -141,7 +141,7 @@
 
             <div class="col-md-12 col-lg-4 sidebar">
               <div class="sidebar-box search-form-wrap">
-                <form action="search.php" class="search-form" method="post">
+                <form action="./ chesearch.php" class="search-form" method="post">
                   <div class="form-group">
                     <span class="icon fa fa-search"></span>
                     <input type="text" name="search" class="form-control" id="s" placeholder="Type a keyword and hit enter">
