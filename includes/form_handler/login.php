17e27698a135d1091d1c100578aee545a9c0a8a0
william Harty <harryalph9@gmail.com>
2022-07-28 09:52:50 +0100

    deleted a variable from the login.php file in the includes/form_handler/login.php  path

--- includes/form_handler/login.php
+++ includes/form_handler/login.php
@@ -1,7 +1,7 @@
 <?php
  session_start();
  $sql = new mysqli("Localhost","root","","content_management_system") or die("Could not connect".mysqli_error($connect));
- $error = [];
+ 
 if(isset($_POST['login_submit_button'])) {
   $cms_email = mysqli_real_escape_string($sql,$_POST['cms_email']);
   $cms_password = mysqli_real_escape_string($sql,$_POST['cms_password']);
