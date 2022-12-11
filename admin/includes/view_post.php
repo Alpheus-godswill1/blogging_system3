2b03975e76131eb6c5e7fdad0962c201fd1b98a6
Alpheus-godswill1 <harryalph9@gmail.com>
2022-12-11 08:40:37 +0100

    modified some stuffs in admin/includes/view_post.php

--- admin/includes/view_post.php
+++ admin/includes/view_post.php
@@ -1,5 +1,5 @@
 <div class="table-responsive">
-<table class="table table-bordered table-striped table-hover">
+<table class="table table-bordered table-striped table-hover ">
   <thead>
     <th>Post ID</th>
     <th>Post Title</th>
